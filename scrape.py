94c45b704f118bb1551f72087fa821f536c3e596
Sunny Miglani <sm15504@my.bristol.ac.uk>
2018-06-29 15:42:45 +0100

    will go through all the summary links

--- scrape.py
+++ scrape.py
@@ -1,10 +1,15 @@
 from bs4 import BeautifulSoup;
 import requests;
 
+listOfURLs = []
 
 def runCrawler(url):
+    ## global variables reference ##
+    global listOfURLs;
+    
     # Returns the webpage in the same format a browser would receive it. Sends a GET request.
     page = requests.get(url);
+    print("Main url : " + str(url));
 
     # Create a soup object that parses the html.
     # This creates a structure to the html page that can be parsed and traversed via soup
@@ -22,13 +27,21 @@ def runCrawler(url):
             list_of_class = p_tag['class'];
             for buzzWord in ignoredBuzzWords:
                 if(buzzWord in list_of_class): continue;
-            if("promo-panel__inner__body" in list_of_class): continue;
         raw_text = p_tag.text;
         if("Sign in" in raw_text): continue;
         outputText += p_tag.text;
         outputText +="\n";
     
     
+    # Grabbing other URLs from the page.
+    all_a_links = soup.findAll("a", {"class": "other-guides__link"});
+    for a_link in all_a_links:
+        link = a_link['href'];
+        link = "https://www.bbc.com" + link;
+        if( link not in listOfURLs):
+            listOfURLs.append(link);
+            print("Just appened link : {0}".format(link));
+        else: continue;
 
     return outputText;
 
@@ -36,12 +49,21 @@ def runCrawler(url):
 
 
 def goThroughListOfURLs():
-    f = open("url.txt", "r") #opens file with name of "test.txt"
-    for line in f:
+    ## global list of urls ## 
+    global listOfURLs;
+
+    with open("url.txt", "r") as f:
+        listOfURLs = f.read().splitlines();
+    for line in listOfURLs:
+        # print(line);
         if(line == "" or line == " " or line == "\n"): continue; ## for extra newline characters.
         if(line[-1] == "\n"):
             line = line[:-1]; # This is basically removing the newline at the end of the line! 
         thisOutput = runCrawler(line);
         print(thisOutput);
-    
+
+    with open("url.txt", "w+") as f:
+        for url in listOfURLs:
+            f.write("%s\n" % url);
+
 goThroughListOfURLs();
